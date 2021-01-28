0feaf436cc3890aa3961e752503c3a61e6f45d7e
souvenir718 <mongkey0@naver.com>
2021-01-29 08:36:25 +0900

    img 확장자

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import "./App.css";
-import { Grid } from "semantic-ui-react";
-import React, { Component } from "react";
-import HeaderContainer from "./Header/HeaderContainer";
-import Footer from "./Footer/Footer";
-import LoginContainer from "./User/Container/LoginContainer";
-import Main from "./MainPage/Main";
-import BasketContainer from "./Basket/BasketContainer";
-import { inject, observer } from "mobx-react";
-import SignUpContainer from "./User/Container/SignUpContainer";
+import './App.css';
+import { Grid } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import HeaderContainer from './Header/HeaderContainer';
+import Footer from './Footer/Footer';
+import LoginContainer from './User/Container/LoginContainer';
+import Main from './MainPage/Main';
+import BasketContainer from './Basket/BasketContainer';
+import { inject, observer } from 'mobx-react';
+import SignUpContainer from './User/Container/SignUpContainer';
 
-
-
-@inject("MainStore")
-@observer
 /*Header, Container: Main, Login, Basket, Signup, Footer 설정*/
+@inject('MainStore')
+@observer
 class App extends Component {
-  render() {
-    //
-    let view = this.props.MainStore.getMainView;
-    return (
-      <Grid>
-        <Grid.Row>
-          <Grid.Column>
-            <HeaderContainer />
-          </Grid.Column>
-        </Grid.Row>
-        {/* Main.js의 메인 부분을 가져옴 */}
-        {view === "Home" && <Main />}
-        {view === "Login" && <LoginContainer />}
-        {view === "Basket" && <BasketContainer />}
-        {view === "SignUp" && <SignUpContainer />}
+    render() {
+        //
+        let view = this.props.MainStore.getMainView;
+        return (
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column>
+                        <HeaderContainer />
+                    </Grid.Column>
+                </Grid.Row>
+                {/* Main.js의 메인 부분을 가져옴 */}
+                {view === 'Home' && <Main />}
+                {view === 'Login' && <LoginContainer />}
+                {view === 'Basket' && <BasketContainer />}
+                {view === 'SignUp' && <SignUpContainer />}
 
-        <Grid.Row style={{ paddingBottom: 0 }}>
-          <Grid.Column>
-            <Footer />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
-  }
+                <Grid.Row style={{ paddingBottom: 0 }}>
+                    <Grid.Column>
+                        <Footer />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
+    }
 }
 
 export default App;
