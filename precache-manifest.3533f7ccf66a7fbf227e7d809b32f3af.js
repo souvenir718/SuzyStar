88ecc0a630b67f5c0d55fadb28e2d1a11a4221e7
souvenir718 <mongkey0@naver.com>
2021-01-29 08:23:49 +0900

    Updates

---
+++ precache-manifest.3533f7ccf66a7fbf227e7d809b32f3af.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d25a750cf50804f1453ae11c2d679e",
+    "url": "/SuzyStar/index.html"
+  },
+  {
+    "revision": "d5b758dd6c6abc96b1f0",
+    "url": "/SuzyStar/static/css/2.06492505.chunk.css"
+  },
+  {
+    "revision": "820e46a5e471a4b00646",
+    "url": "/SuzyStar/static/css/main.ee088f95.chunk.css"
+  },
+  {
+    "revision": "d5b758dd6c6abc96b1f0",
+    "url": "/SuzyStar/static/js/2.1642fa85.chunk.js"
+  },
+  {
+    "revision": "35ba3478a9b6a438bda6a01d78de9e44",
+    "url": "/SuzyStar/static/js/2.1642fa85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820e46a5e471a4b00646",
+    "url": "/SuzyStar/static/js/main.27eccb50.chunk.js"
+  },
+  {
+    "revision": "debb20f31802d2e6ecf1",
+    "url": "/SuzyStar/static/js/runtime-main.d8d8783f.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/SuzyStar/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/SuzyStar/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/SuzyStar/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/SuzyStar/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/SuzyStar/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/SuzyStar/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/SuzyStar/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/SuzyStar/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/SuzyStar/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/SuzyStar/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/SuzyStar/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/SuzyStar/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/SuzyStar/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/SuzyStar/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/SuzyStar/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/SuzyStar/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
