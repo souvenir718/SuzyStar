db931c9c8726db0fe5d9decc52f3565e6ca089d4
souvenir718 <mongkey0@naver.com>
2021-01-29 08:25:22 +0900

    deploy gh-page test

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Provider } from "mobx-react";
-import "semantic-ui-css/semantic.min.css";
-import ProductStore from "./Product/Store/ProductStore";
-import UserStore from "./User/Store/UserStore";
-import MainStore from "./MainPage/MainStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'mobx-react';
+import 'semantic-ui-css/semantic.min.css';
+import ProductStore from './Product/Store/ProductStore';
+import UserStore from './User/Store/UserStore';
+import MainStore from './MainPage/MainStore';
 
 ReactDOM.render(
-  <Provider
-    MainStore={MainStore}
-    ProductStore={ProductStore}
-    UserStore={UserStore}
-  >
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider MainStore={MainStore} ProductStore={ProductStore} UserStore={UserStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
